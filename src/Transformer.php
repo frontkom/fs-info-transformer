5318a0da2644f86c7765fddaa423b7fa112b8036
Eirik Stanghelle Morland <eirik@morland.no>
2024-09-30 12:08:01 +0200

    Add weblink transform and tests for it (#1)

--- src/Transformer.php
+++ src/Transformer.php
@@ -4,8 +4,30 @@ namespace Frontkom\FsInfoTransformer;
 
 class Transformer
 {
-    public function transform($data)
+    public function transform(string $data) : string
     {
+        $data = $this->processWeblinks($data);
         return $data;
+    }
+
+    public function processWeblinks(string $data, bool $nofollow = true) : string
+    {
+        // The changes should be like this. An input would be
+        // <webLink><href>https://www.example.com</href><linkName>gjeldende forskrift og tilhørende retningslinjer.
+        // </linkName></webLink>.
+        // And then the output should become
+        // <a rel="nofollow" href="https://www.example.com">gjeldende forskrift og tilhørende retningslinjer.</a>
+        if ($nofollow) {
+            return preg_replace(
+                '/<webLink><href>(.*?)<\/href>(<linkName>(.*?)<\/linkName>)?<\/webLink>/',
+                '<a rel="nofollow" href="$1">$3</a>',
+                $data,
+            );
+        }
+        return preg_replace(
+            '/<webLink><href>(.*?)<\/href>(<linkName>(.*?)<\/linkName>)?<\/webLink>/',
+            '<a href="$1">$3</a>',
+            $data,
+        );
     }
 }
---
+++ tests/WebLinksTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Frontkom\FsInfoTransformer\Tests;
+
+use Frontkom\FsInfoTransformer\Transformer;
+use PHPUnit\Framework\TestCase;
+
+class WebLinksTest extends TestCase
+{
+    /**
+     * Test weblinks.
+     *
+     * Basically with and without nofollow.
+     *
+     * @dataProvider weblinksProvider
+     */
+    public function testWeblinks($input, $expected, $nofollow)
+    {
+        $transformer = new Transformer();
+        $result = $transformer->processWeblinks($input, $nofollow);
+        $this->assertEquals($expected, $result);
+    }
+
+    public static function weblinksProvider()
+    {
+        return [
+            [
+                // @phpcs:ignore
+                '<webLink><href>https://www.example.com</href><linkName>gjeldende forskrift og tilhørende retningslinjer.</linkName></webLink>',
+                // @phpcs:ignore
+                '<a rel="nofollow" href="https://www.example.com">gjeldende forskrift og tilhørende retningslinjer.</a>',
+                true,
+            ],
+            [
+                // @phpcs:ignore
+                '<webLink><href>https://www.example.com</href><linkName>gjeldende forskrift og tilhørende retningslinjer.</linkName></webLink>',
+                '<a href="https://www.example.com">gjeldende forskrift og tilhørende retningslinjer.</a>',
+                false,
+            ],
+        ];
+    }
+}
