753e399246b72035cd9a1416aeed892c24d578ad
Eirik S. Morland <eirik@morland.no>
2024-09-30 09:48:28 +0200

    Run tests

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -5,7 +5,8 @@ namespace Frontkom\FsInfoTransformer\Tests;
 use Frontkom\FsInfoTransformer\Transformer;
 use PHPUnit\Framework\TestCase;
 
-class UnitTest extends TestCase {
+class UnitTest extends TestCase
+{
 
   /**
    * Test all the cases defined in a JSON file with cases.
@@ -15,17 +16,18 @@ class UnitTest extends TestCase {
    *
    * @dataProvider casesProvider
    */
-  public function testAllCases($input, $expected) {
-    $transformer = new Transformer();
-    $actual = $transformer->transform($input);
-    $this->assertEquals($expected, $actual);
-  }
+    public function testAllCases($input, $expected)
+    {
+        $transformer = new Transformer();
+        $actual = $transformer->transform($input);
+        $this->assertEquals($expected, $actual);
+    }
 
-  public static function casesProvider() {
-    $cases = json_decode(file_get_contents(__DIR__ . '/assets/cases.json'), true);
-    return array_map(function ($case) {
-      return $case;
-    }, $cases);
-  }
-
+    public static function casesProvider()
+    {
+        $cases = json_decode(file_get_contents(__DIR__ . '/assets/cases.json'), true);
+        return array_map(function ($case) {
+            return $case;
+        }, $cases);
+    }
 }
